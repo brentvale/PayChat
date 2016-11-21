d9b88ff28e8f718b25643bfde97b78c2d0683b26
Brent Vale <brentvale@gmail.com>
2016-11-20 19:12:17 -0800

    adds validations for presence and length of names [Finishes #134644489]

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, :length => { :maximum => 30 }
+  validates :last_name, :length => { :maximum => 30 }
 end
